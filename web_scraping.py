6dcc7b1364f44a8c8821ee39e600866653646a4c
KhalilAmamriDev <KhalilAmamri@outlook.com>
2025-06-17 21:08:55 +0100

    Add job links to CSV export in web scraping script

--- web_scraping.py
+++ web_scraping.py
@@ -7,6 +7,8 @@ job_titles = []
 company_names = []
 location_names = []
 job_skills = []
+links = []
+
 
 
 result = requests.get("https://wuzzuf.net/search/jobs/?q=Python&a=hpb")
@@ -22,14 +24,18 @@ Vjob_skills = soup.find_all("div", {"class":"css-y4udm8"})
 
 for i in range(len(Vjobs_titles)):
     job_titles.append(Vjobs_titles[i].text)
+    links.append(Vjobs_titles[i].find("a").attrs['href'])
     company_names.append(Vcompany_names[i].text)
     location_names.append(Vlocation_names[i].text)
     job_skills.append(Vjob_skills[i].text)
 
+file_list = [job_titles, company_names, location_names, job_skills, links]
+exported = zip_longest(*file_list)
+
 with open("C:\\Learn Programming\\Web Scraping with Python\\jobstutorial.csv","w") as myfile:
     wr = csv.writer(myfile)
-    wr.writerow(["Job Title", "Company Name", "Location Name", "Job Skills"])
-
+    wr.writerow(["Job Title", "Company Name", "Location Name", "Job Skills", "Links"])
+    wr.writerows(exported)
 
 
 
